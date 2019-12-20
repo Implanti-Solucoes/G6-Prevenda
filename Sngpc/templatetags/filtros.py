6fb31e01e4f03e4986e3916c426a673b358162e1
Michael Douglas Pereira de Morais <maiconkkl@users.noreply.github.com>
2019-12-20 10:26:55 -0300

    Removendo edição SNGPC
    Foi removido os fields do sngpc e foi colocado algumas validações com alterações visuais para evitar problemas de envio.

--- Sngpc/templatetags/filtros.py
+++ Sngpc/templatetags/filtros.py
@@ -1,11 +1,80 @@
 from django import template
+from django.template.defaultfilters import stringfilter
 from Estoque.models import Products
+from datetime import datetime
+
 register = template.Library()
 
 
 @register.filter
+@stringfilter
 def busca_produto(value):
     db = Products
     produto = db.get_produto_servico(ms=value)
     if len(produto) == 1:
         return produto[0]['Descricao']
+    return "Não encontrado"
+
+
+@register.filter
+@stringfilter
+def busca_produto_anvisa(value, ms_dict):
+    if value in ms_dict:
+        return ms_dict[value]
+    return "Não encontrado"
+
+
+@register.filter
+@stringfilter
+def busca_produto_anvisa_valida(value, ms_dict):
+    if value not in ms_dict:
+        return 'class="bg-danger"'
+    return ""
+
+
+@register.filter
+@stringfilter
+def valida_data_start(date_document: str, date_file: str):
+    date_document_a1 = datetime.strptime(date_document, "%Y-%m-%d").date()
+    date_file_a1 = datetime.strptime(str(date_file), "%Y-%m-%d").date()
+    if date_document_a1 < date_file_a1:
+        return ' bg-danger'
+    return ''
+
+
+@register.filter
+@stringfilter
+def valida_data_end(date_document: str, date_file: str):
+    date_document_a1 = datetime.strptime(date_document, "%Y-%m-%d").date()
+    date_file_a1 = datetime.strptime(str(date_file), "%Y-%m-%d").date()
+    if date_document_a1 > date_file_a1:
+        return ' bg-danger'
+    return ''
+
+
+@register.filter
+@stringfilter
+def valida_ms(ms: str):
+    if len(ms) != 13:
+        return ' class="bg-danger"'
+    return ''
+
+
+@register.filter
+@stringfilter
+def valida_notificao(notificacao: str, tipo_receituario_medicamento: int):
+    notificacao = notificacao.replace(' ', '')
+    if tipo_receituario_medicamento == 2 and notificacao == '' or \
+            tipo_receituario_medicamento == 3 and notificacao == '' or \
+            tipo_receituario_medicamento == 4 and notificacao == '':
+        return ' bg-danger'
+    return ''
+
+
+@register.filter
+@stringfilter
+def valida_conselho(conselho: str, uso: int):
+    conselho = conselho.lower()
+    if conselho == 'crmv' and uso == 1:
+        return ' bg-danger'
+    return ''
--- Sngpc/views.py
+++ Sngpc/views.py
@@ -4,7 +4,8 @@ from lxml import objectify
 import hashlib
 import glob
 import os
-
+import requests
+from selectolax.parser import HTMLParser
 
 def index(request):
     # variaveis padrões
@@ -26,12 +27,55 @@ def index(request):
 def listagem(request, file):
     # variaveis padrões
     template_name = 'sngpc/listagem.html'
-    context = {
-        'items': []
-    }
     dir = 'C:\\DigiSat\\SuiteG6\\Sistema\\Sngpc\\'
+    ms = []
+    ms_dict = {}
+    session = requests.session()
     for item in glob.glob(os.path.join(dir, '*.xml')):
         if file == str(hashlib.md5(open(item, 'rb').read()).hexdigest()):
             rawdata = open(item, 'r').read()
             file = objectify.fromstring(str(rawdata).encode('utf-8'))
-            return render(request, template_name, {'file': file})
+            for entrada in file.corpo.medicamentos.entradaMedicamentos:
+                for medicamento in entrada.medicamentoEntrada:
+                    if str(medicamento.registroMSMedicamento) not in ms:
+                        ms.append(str(medicamento.registroMSMedicamento))
+
+            for saida in file.corpo.medicamentos.saidaMedicamentoVendaAoConsumidor:
+                try:
+                    for medicamento in saida.medicamentoVenda:
+                        if str(medicamento.registroMSMedicamento) not in ms:
+                            ms.append(str(medicamento.registroMSMedicamento))
+                except AttributeError:
+                    pass
+            for x in ms:
+                if len(x) == 13:
+                    headers = {
+                        'Connection': 'keep-alive',
+                        'Cache-Control': 'max-age=0',
+                        'Origin': 'http://sngpc.anvisa.gov.br',
+                        'Upgrade-Insecure-Requests': '1',
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36 OPR/65.0.3467.72',
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+                        'Referer': 'http://sngpc.anvisa.gov.br/ConsultaMedicamento/index.asp',
+                        'Accept-Encoding': 'gzip, deflate',
+                        'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+                    }
+
+                    data = {
+                        'NU_REG': '{}.{}.{}.{}-{}'.format(
+                            x[0:1], x[1:5], x[5:9], x[9:12], x[12:13]
+                        )
+                    }
+
+                    response = session.post(
+                        'http://sngpc.anvisa.gov.br/ConsultaMedicamento/index.asp',
+                        headers=headers, data=data, verify=False)
+                    response_data = HTMLParser(response.text).tags('td')
+                    dados_ms = response_data[20].text(deep=True, separator='', strip=False)
+                    dados_ms = dados_ms.replace('\n', '')
+                    dados_ms = dados_ms.replace('	', '')
+                    dados_ms = dados_ms.replace('  ', '')
+                    dados_ms = dados_ms.replace(' Apresentação Comercial do Medicamento: ', '')
+                    ms_dict[x] = dados_ms
+            return render(request, template_name, {'file': file, 'ms_dict': ms_dict})
